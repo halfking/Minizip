8361959d5e4162455aa61f8d4820d2dd60e45ed7
HUANGXUTAO <huangxutao@huangxutaodembp.lan>
2016-04-28 12:56:09 +0800

    remove

--- Minizip/HCMinizip.h
+++ Minizip/HCMinizip.h
@@ -1,32 +1,13 @@
 //
-//  Minizip.h
-//  Minizip
+//  HCMinizip.h
+//  HCMinizip
 //
-//  Created by Arthur Dexter on 2/6/16.
-//  Copyright © 2016 Arthur Dexter. All rights reserved.
-//
-//  This software may be modified and distributed under the terms
-//  of the MIT license.  See the LICENSE file for details.
+//  Created by HUANGXUTAO on 16/4/28.
+//  Copyright © 2016年 Arthur Dexter. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#ifndef HCMinizip_h
+#define HCMinizip_h
 
-//! Project version number for Minizip.
-FOUNDATION_EXPORT double MinizipVersionNumber;
 
-//! Project version string for Minizip.
-FOUNDATION_EXPORT const unsigned char MinizipVersionString[];
-
-#import <HCMinizip/ioapi.h>
-#import <HCMinizip/ioapi_buf.h>
-#import <HCMinizip/ioapi_mem.h>
-#import <HCMinizip/unzip.h>
-#import <HCMinizip/zip.h>
-#import <HCMinizip/NSDataGZipAdditions.h>
-
-#import <HCMinizip/FileInZipInfo.h>
-#import <HCMinizip/ZipException.h>
-#import <HCMinizip/ZipFile.h>
-#import <HCMinizip/ZipReadStream.h>
-#import <HCMinizip/ZipWriteStream.h>
-
+#endif /* HCMinizip_h */
